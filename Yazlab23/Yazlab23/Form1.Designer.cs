68f9d56a2ec380d0d2bbd1a421dce9d43cb20b4d
atakanibis <33881777+atakanibis@users.noreply.github.com>
2021-02-03 15:18:13 +0300

    Add files via upload

---
+++ Yazlab23/Yazlab23/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Yazlab23
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.nodeFrom = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nodeTo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.capacity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Add.Location = new System.Drawing.Point(119, 12);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 21);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nodeFrom,
+            this.nodeTo,
+            this.capacity});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(200, 39);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(245, 257);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // nodeFrom
+            // 
+            this.nodeFrom.Text = "Node From";
+            this.nodeFrom.Width = 73;
+            // 
+            // nodeTo
+            // 
+            this.nodeTo.Text = "Node To";
+            this.nodeTo.Width = 77;
+            // 
+            // capacity
+            // 
+            this.capacity.Text = "Capacity";
+            this.capacity.Width = 91;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(200, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(258, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(316, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(52, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(373, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 21);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(119, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 27);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.id,
+            this.name});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(181, 256);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // id
+            // 
+            this.id.Text = "ID";
+            // 
+            // name
+            // 
+            this.name.Text = "Name";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 307);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(45, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(68, 307);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(45, 21);
+            this.comboBox2.TabIndex = 11;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 342);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader nodeFrom;
+        private System.Windows.Forms.ColumnHeader nodeTo;
+        private System.Windows.Forms.ColumnHeader capacity;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader id;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Yazlab23/Yazlab23/Form1.cs
@@ -0,0 +1,87 @@
+﻿using FordFulkersonConsole;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yazlab23
+{
+    public partial class Form1 : Form
+    {
+        int _counter = 1;
+        Dictionary<int, string> Nodes = new Dictionary<int, string>();
+        List<Edge> Edges = new List<Edge>();
+        int _start, _end = 0;
+        public class Edge
+        {
+            public int nodeFrom { get; set; }
+            public int nodeTo { get; set; }
+            public int capacity { get; set; }
+            public override string ToString()
+            {
+                return String.Format("{0} {1} {2}", nodeFrom, nodeTo, capacity);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            ListViewItem row = new ListViewItem(_counter.ToString());
+            row.SubItems.Add(textBox1.Text);
+            listView1.Items.Add(row);
+            Nodes.Add(_counter, textBox1.Text);
+            comboBox1.Items.Add(textBox1.Text);
+            comboBox2.Items.Add(textBox1.Text);
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = comboBox2.Items.Count - 1;
+            textBox1.Text = "";
+            _counter++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListViewItem row = new ListViewItem(textBox2.Text);
+            row.SubItems.Add(textBox3.Text);
+            row.SubItems.Add(textBox4.Text);
+            listView2.Items.Add(row);
+            var nodeFromId = Nodes.FirstOrDefault(x => x.Value == textBox2.Text).Key;
+            var nodeToId = Nodes.FirstOrDefault(x => x.Value == textBox3.Text).Key;
+            Edges.Add(new Edge() { nodeFrom = nodeFromId, nodeTo = nodeToId, capacity = Int32.Parse(textBox4.Text)});
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string names = string.Join(" ", Nodes.Values.ToArray());
+            string[] edges = new string[Edges.Count];
+            int i = 0;
+            foreach(var edge in Edges)
+            {
+                edges[i++] = edge.ToString();
+            }
+            var solver = new ProblemSolver(names, edges, _start, _end);
+            solver.Run();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _start = comboBox1.SelectedIndex + 1;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _end = comboBox2.SelectedIndex + 1;
+        }
+
+    }
+}
